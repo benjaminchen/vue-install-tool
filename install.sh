590a19fd757d56ea1795b3afccebca84a3d11075
benjamin <benjaminchen.c@gmail.com>
2017-10-31 11:31:57 +0800

    add json package to edit package.json file

--- install.sh
+++ install.sh
@@ -41,47 +41,13 @@ echo "${GREEN}$author${NORMAL}\n"
 git clone https://github.com/benjaminchen/vue-install-tool.git $project
 cd ./$project
 
-echo "{
-  \"name\": \"$project\",
-  \"version\": \"0.0.1\",
-  \"description\": \"$description\",
-  \"main\": \"index.js\",
-  \"scripts\": {
-    \"test\": \"echo \\\"Error: no test specified\\\" && exit 1\",
-    \"build\": \"webpack\",
-    \"watch\": \"webpack --watch\",
-    \"dev\": \"webpack && webpack-dev-server\",
-    \"production\": \"NODE_ENV=production webpack\"
-  },
-  \"author\": \"$author\",
-  \"license\": \"ISC\",
-  \"devDependencies\": {
-    \"clean-webpack-plugin\": \"^0.1.17\",
-    \"copy-webpack-plugin\": \"^4.1.0\",
-    \"css-loader\": \"^0.28.7\",
-    \"extract-text-webpack-plugin\": \"^3.0.0\",
-    \"html-loader\": \"^0.5.1\",
-    \"html-webpack-plugin\": \"^2.30.1\",
-    \"jquery\": \"^3.2.1\",
-    \"lodash\": \"^4.17.4\",
-    \"node-sass\": \"^4.5.3\",
-    \"pug-html-loader\": \"^1.1.5\",
-    \"sass-loader\": \"^6.0.6\",
-    \"style-loader\": \"^0.18.2\",
-    \"uglifyjs-webpack-plugin2\": \"^1.0.3\",
-    \"vue\": \"^2.4.4\",
-    \"vue-loader\": \"^13.0.5\",
-    \"vue-material\": \"^0.7.5\",
-    \"vue-router\": \"^3.0.1\",
-    \"vue-template-compiler\": \"^2.4.4\",
-    \"webpack\": \"^3.6.0\",
-    \"webpack-dev-server\": \"^2.9.3\"
-  }
-}" > package.json
-
 echo "# $project" > README.md
 
 npm install
+
+updateJson="./node_modules/.bin/json -I -f package.json -e 'this.name=\"$project\",this.description=\"$description\",this.author=\"$author\"'"
+
+eval $updateJson
 
 rm -rf ./.git
 
